498dd647d943af39975ba07559f19b30edfad79a
Yassine Chebil <yassinechebil@parwx0206402.home>
2018-01-15 12:02:55 +0100

    - Add Common NetworkService for all Requests with a generic method for fetching data

--- CatTestProject/CatTestProject/Services/Services/Commons/NetworkService.swift
+++ CatTestProject/CatTestProject/Services/Services/Commons/NetworkService.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2018 Yassine Chebil. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+class NetworkService {
+    
+    // MARK:- Variables & Constants
+    static let sharedInstance = NetworkService()
+    
+    
+    // MARK:- Methods
+    
+    //Generic func for sending all API Requests
+    func sendDataFetchRequest(url:String,
+                              methodType:HTTPMethod,
+                              params:Parameters,
+                              urlEncoding:ParameterEncoding,
+                              headers:HTTPHeaders?,
+                              callbackSuccess: @escaping (JSON) -> Void,
+                              callbackFailure:@escaping (AFError) -> Void)
+    {
+        
+        //usage of alamofire Global server configuration for sending request
+        ServerConfiguration.manager.request(url,
+                                            method: methodType,
+                                            parameters: params,
+                                            encoding: urlEncoding,
+                                            headers: headers)
+            
+            .validate()
+            .responseJSON { response in
+                
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    callbackSuccess(json)
+                case .failure:
+                    if let error: AFError = response.result.error as? AFError {
+                        callbackFailure(error)
+                    }
+                    
+                }
+        }
+        
+    }
+}
