5a576947465bf1d4f3d418956bfcf7f8701d618f
Yassine Chebil <yassinechebil@parwx0206402.home>
2018-01-15 14:35:05 +0100

    - Add Cat model to parse array of Cats from API

---
+++ CatTestProject/CatTestProject/Modules/Views/Vote/VoteCuteCatViewController.swift
@@ -0,0 +1,32 @@
+//
+//  VoteCuteCatViewController.swift
+//  CatTestProject
+//
+//  Created by Yassine Chebil on 15/01/2018.
+//  Copyright © 2018 Yassine Chebil. All rights reserved.
+//
+
+import UIKit
+
+class VoteCuteCatViewController: UIViewController {
+
+    //MARK: - Variables & Constants
+    var catsArray = [Cat]()
+    
+    
+    //MARK: - UIViewController lifeCycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+}
---
+++ CatTestProject/CatTestProject/Services/Services/Vote/Models/Cat.swift
@@ -0,0 +1,29 @@
+//
+//  Cat.swift
+//  CatTestProject
+//
+//  Created by Yassine Chebil on 15/01/2018.
+//  Copyright © 2018 Yassine Chebil. All rights reserved.
+//
+
+import SwiftyJSON
+
+class Cat: NSObject {
+    
+    var catId: String?
+    var catImageUrl: String?
+    var catScore: Int?
+    
+    override init() {}
+    
+    init(jsonObject:JSON){
+        
+        if let catId = jsonObject["id"].string  {
+            self.catId = catId
+        }
+        
+        if let catImageUrl = jsonObject["url"].string  {
+            self.catImageUrl = catImageUrl
+        }
+    }
+}
