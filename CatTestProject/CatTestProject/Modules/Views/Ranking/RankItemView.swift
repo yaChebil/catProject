5e372b0cac31d30677ddf3a9985f16aea8efc907
Yassine Chebil <yassinechebil@parwx0206402.home>
2018-01-17 10:17:56 +0100

    - Add custom view for carousel items - setup design and configure the view - Add usage of RankItemView on RankingCatsViewController

---
+++ CatTestProject/CatTestProject/Modules/Views/Ranking/RankItemView.swift
@@ -0,0 +1,16 @@
+//
+//  RankItemView.swift
+//  CatTestProject
+//
+//  Created by Yassine Chebil on 17/01/2018.
+//  Copyright © 2018 Yassine Chebil. All rights reserved.
+//
+
+import UIKit
+
+class RankItemView: UIView {
+
+    @IBOutlet weak var catImageView: UIImageView!
+    @IBOutlet weak var catScoreLabel: UILabel!
+
+}
--- CatTestProject/CatTestProject/Modules/Views/Ranking/RankingCatsViewController.swift
+++ CatTestProject/CatTestProject/Modules/Views/Ranking/RankingCatsViewController.swift
@@ -45,35 +45,38 @@ class RankingCatsViewController: UIViewController, iCarouselDataSource, iCarouse
         var label: UILabel
         var itemView: UIImageView
         
-        //reuse view if available, otherwise create a new view
-        if let view = view as? UIImageView {
-            itemView = view
-            //get a reference to the label in the recycled view
-            label = itemView.viewWithTag(1) as! UILabel
-        } else {
-            //don't do anything specific to the index within
-            //this `if ... else` statement because the view will be
-            //recycled and used with other index values later
-            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-            itemView.image = UIImage(named: "page.png")
-            itemView.contentMode = .center
-            
-            label = UILabel(frame: itemView.bounds)
-            label.backgroundColor = .clear
-            label.textAlignment = .center
-            label.font = label.font.withSize(50)
-            label.tag = 1
-            itemView.addSubview(label)
-        }
+        let rankItemView = Bundle.main.loadNibNamed("RankItemView", owner: self, options: nil)!.first as! RankItemView
+        rankItemView.catScoreLabel.text = "17 votes"
+        
+//        //reuse view if available, otherwise create a new view
+//        if let view = view as? UIImageView {
+//            itemView = view
+//            //get a reference to the label in the recycled view
+//            label = itemView.viewWithTag(1) as! UILabel
+//        } else {
+//            //don't do anything specific to the index within
+//            //this `if ... else` statement because the view will be
+//            //recycled and used with other index values later
+//            itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+//            itemView.image = UIImage(named: "page.png")
+//            itemView.contentMode = .center
+//
+//            label = UILabel(frame: itemView.bounds)
+//            label.backgroundColor = .clear
+//            label.textAlignment = .center
+//            label.font = label.font.withSize(50)
+//            label.tag = 1
+//            itemView.addSubview(label)
+//        }
         
         //set item label
         //remember to always set any properties of your carousel item
         //views outside of the `if (view == nil) {...}` check otherwise
         //you'll get weird issues with carousel item content appearing
         //in the wrong place in the carousel
-        label.text = "\(items[index])"
+       // label.text = "\(items[index])"
         
-        return itemView
+        return rankItemView
     }
     
     func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
